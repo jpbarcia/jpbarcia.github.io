f3c2ae99f87259c52f064fdda92173ef29f00113
Jean Pierre <jeanpbarcia@gmail.com>
2015-12-28 19:02:17 -0500

    2 Sections completed
    Not responsive yet!

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,30 @@
 
-$(document).ready(function(){
-       $("body").mousewheel(function(event, delta) {
-          this.scrollLeft -= (delta * 30);
-          //event.preventDefault();
-       });
+$(document).ready(
+  function()
+  {
+    $("body").mousewheel(
+    function(event, delta)
+    {
+      this.scrollLeft -= (delta * 30);
+      //event.preventDefault();
+    });
     /*setTimeout(function(){
        window.location.reload(1);
     }, 5000);*/
+
     mobileNav();
-    window.randomize = function() {
-    	$('.skill-container').attr('data-skill', Math.floor(Math.random() * 100));
-      setTimeout(window.randomize, 3000);
+
+    window.randomize = function()
+    {
+    	$('.skill-container').each(function()
+      {
+         $(this).attr('data-skill', Math.floor(Math.random() * 100));
+      });
+      $('.bar').each(function()
+      {
+        $(this).attr('data-bar', Math.floor(Math.random() * 100));
+      });
     }
-    setTimeout(window.randomize, 3000);
     $('.skill-container').click(window.randomize);
 });
 
@@ -22,9 +34,6 @@ function mobileNav() {
     if(status){ $('header').removeClass('is-open'); }
     else {
       $('header').addClass('is-open');
-      //var height = $(document).height();
-      //$('#overlay').css('height', height);
-      //$('body').css('overflow', 'hidden');
     }
   });
   $('a').on('click', function(){
