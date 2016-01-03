4867543fee017b04aa3ae9845a8945461110e0de
Jean Pierre <jeanpbarcia@gmail.com>
2016-01-02 23:29:35 -0500

    Responsive Design
    TODO: Refactoring and some functionalities

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,33 @@
 
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        var targetTop = target.offset().top - parseInt($('.wrapper').css("margin-top"));
+        //alert(targetTop);
+        $('html,body').animate({
+          scrollTop: targetTop
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
 $(document).ready(
 
   function()
   {
-    $("body").mousewheel(
-    function(event, delta)
-    {
-      this.scrollLeft -= (delta * 30);
-      //event.preventDefault();
-    });
-    /*setTimeout(function(){
-       window.location.reload(1);
-    }, 5000);*/
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 800);
-            return false;
-          }
-        }
-      });
-    });
     mobileNav();
-
+    window.addEventListener('scroll', function() {
+      var status = $('header').hasClass('is-open');
+      if(status){
+        $('header').removeClass('is-open');
+        $('#overlay').removeClass('cover-all');
+      }
+     });
     window.randomize = function()
     {
     	$('.skill-container').each(function()
@@ -45,19 +45,26 @@ $(document).ready(
 function mobileNav() {
   $('.small-nav-toggle').on('click', function(){
     var status = $('header').hasClass('is-open');
-    if(status){ $('header').removeClass('is-open'); }
+    if(status){
+      $('header').removeClass('is-open');
+      $('#overlay').removeClass('cover-all');
+    }
     else {
       $('header').addClass('is-open');
+      $('#overlay').addClass('cover-all');
     }
   });
   $('a').on('click', function(){
     var status = $('header').hasClass('is-open');
     if(status)
       $('header').removeClass('is-open');
+      $('#overlay').removeClass('cover-all');
+
   });
   $('#overlay').on('click', function(){
     var status = $('header').hasClass('is-open');
     if(status)
       $('header').removeClass('is-open');
+      $('#overlay').removeClass('cover-all');
   });
 }
